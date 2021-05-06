96e7669578ff98ad3a0917a294421e809aad8759
Andres Huerta <a17300123@ceti.mx>
2021-05-06 03:14:54 -0500

    Record screen hardcoded and icons package added

--- src/screens/menu_screens/Record.js
+++ src/screens/menu_screens/Record.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {XIcon, PinIcon} from '@primer/octicons-react'
 import "./Record.css"
 
 import Navbar from "../menu_components/Navbar";
@@ -8,7 +9,88 @@ const Record = () => {
         <div>
             <Navbar />
             <div className="jumbotron">
-                <h1>Record</h1>
+                <h1>Record</h1><br></br>
+
+                <div class="list-group">
+                  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">y' + 2xy = 0</h5>
+                      <small>  
+                        <button class="btn bg-transparent">
+                          <XIcon size={24} />
+                        </button>  
+                        <button class="btn bg-transparent">
+                          <PinIcon size={24} />    
+                        </button> 
+                      </small>
+                    </div>
+                    <p class="mb-1">Sunday 4, April 18:25</p>
+                  </a>
+                </div>
+                <div class="list-group">
+                  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">y' + 2xy = 0</h5>
+                      <small>  
+                        <button class="btn bg-transparent">
+                          <XIcon size={24} />
+                        </button>  
+                        <button class="btn bg-transparent">
+                          <PinIcon size={24} />    
+                        </button> 
+                      </small>
+                    </div>
+                    <p class="mb-1">Sunday 4, April 18:25</p>
+                  </a>
+                </div>
+                <div class="list-group">
+                  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">y' + 2xy = 0</h5>
+                      <small>  
+                        <button class="btn bg-transparent">
+                          <XIcon size={24} />
+                        </button>  
+                        <button class="btn bg-transparent">
+                          <PinIcon size={24} />    
+                        </button> 
+                      </small>
+                    </div>
+                    <p class="mb-1">Sunday 4, April 18:25</p>
+                  </a>
+                </div>
+                <div class="list-group">
+                  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">y' + 2xy = 0</h5>
+                      <small>  
+                        <button class="btn bg-transparent">
+                          <XIcon size={24} />
+                        </button>  
+                        <button class="btn bg-transparent">
+                          <PinIcon size={24} />    
+                        </button> 
+                      </small>
+                    </div>
+                    <p class="mb-1">Sunday 4, April 18:25</p>
+                  </a>
+                </div>
+                <div class="list-group">
+                  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                      <h5 class="mb-1">y' + 2xy = 0</h5>
+                      <small>  
+                        <button class="btn bg-transparent">
+                          <XIcon size={24} />
+                        </button>  
+                        <button class="btn bg-transparent">
+                          <PinIcon size={24} />    
+                        </button> 
+                      </small>
+                    </div>
+                    <p class="mb-1">Sunday 4, April 18:25</p>
+                  </a>
+                </div>
             </div>
         </div>
     );
---
+++ src/screens/menu_screens/record_components/Ecuation.js
@@ -0,0 +1,22 @@
+/*import React from "react";
+import {XIcon, PinIcon} from '@primer/octicons-react'
+import "./Ecuation.css"
+
+const Ecuation = () => {
+    return (
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">y' + 2xy = 0</h5>
+            <small>  
+              <button class="btn bg-transparent">
+                <XIcon size={24} />
+              </button>  
+              <button class="btn bg-transparent">
+                <PinIcon size={24} />    
+              </button> 
+            </small>
+          </div>
+          <p class="mb-1">Sunday 4, April 18:25</p>   
+        </a> 
+    )
+}*/
