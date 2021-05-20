b82e66c36fa75c2b864c4a0c92f43cfb0b0030b4
Andres Huerta <a17300123@ceti.mx>
2021-05-20 09:47:07 -0500

    Co-authored-by: David López <buronsuave@users.noreply.github.com>

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -8,7 +8,7 @@ const SignUp = ({ history }) => {
         async event => {
             event.preventDefault();
             const { email, password } = event.target.elements;
-            try {   
+            try {
                 app.auth().createUserWithEmailAndPassword(email.value, password.value);
                 history.push("/");
             } catch (error) {
@@ -24,13 +24,25 @@ const SignUp = ({ history }) => {
                     <legend>Sign Up</legend>
                     <div className="form-group">
                         <label>Email address</label>
-                        <input name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                        <input name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div className="form-group">
                         <label>Password</label>
-                        <input name="password" type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                        <input name="password" type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                     </div>
+                    <div class="form-group">
+                        <label>User type</label>
+                        <select class="custom-select d-block w-100" id="inputGroupSelect" required>
+                            <option value="student">student</option>
+                            <option value="teacher">teacher</option>
+                        </select>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></input>
+                        <label>I want to receive updates in my email</label>
+                    </div>
+                    <br></br>
                     <div className="btn-group-vertical">
                         <button type="submit" className="btn btn-primary">Submit</button><br></br>
                         <button onClick={() => history.push("/login")} type="button" className="btn btn-secondary">Log In</button>
--- src/screens/menu_screens/Settings.js
+++ src/screens/menu_screens/Settings.js
@@ -9,6 +9,32 @@ const Settings = () => {
             <Navbar />
             <div className="jumbotron">
                 <h1>Settings</h1>
+                <form>
+                    <fieldset>
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                        </div>
+                        <div class="form-group">
+                            <label>Example select</label>
+                            <select class="custom-select d-block w-100" id="country" required="">
+                                <option value="">Choose...</option>
+                                <option>United States</option>
+                            </select>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></input>
+                            <label>Default radio</label>
+                        </div>
+                        <br></br>
+                        <button type="button" class="btn btn-primary">Primary</button>
+                    </fieldset>
+                </form>
             </div>
         </div>
     );
